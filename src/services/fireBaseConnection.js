4e2f64f283ca4019989a128e65fea6726416701b
AdemarChamiNeto <adeneto23@hotmail.com>
2023-04-10 19:12:34 -0300

    first commit

---
+++ src/services/fireBaseConnection.js
@@ -0,0 +1,14 @@
+import { initializeApp } from "firebase/app";
+
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDKrrsIdyVOwPYuXW3YTWnst88SzN5bCWA",
+  authDomain: "smoke-f0a56.firebaseapp.com",
+  projectId: "smoke-f0a56",
+  storageBucket: "smoke-f0a56.appspot.com",
+  messagingSenderId: "623658323066",
+  appId: "1:623658323066:web:91b2c9ce43d7e83c8e7fc3",
+  measurementId: "G-8RD1F94924",
+};
+
+const app = initializeApp(firebaseConfig);
